aac4bb11bdc4998fcc7a973597d6ff362ad6fe84
MisonoTakezo <misonotakezo1213@gmail.com>
2021-09-02 12:12:37 +0900

    count  down event handler

--- frontend/src/components/Buttons/CountDownButton.jsx
+++ frontend/src/components/Buttons/CountDownButton.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { RoundButton } from "../shared_style";
 
 export const CountDownButton = ({ onClick, isDisabled }) => (
-  < RoundButton >
+  < RoundButton onClick={onClick} disabled={isDisabled}>
     -
   </RoundButton >
 )
